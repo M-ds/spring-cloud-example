a907c6e6c21ff224f6d9475cf4399eb845ef3dad
Michiel de Smet <Michieldesmet1994@gmail.com>
2021-09-29 13:15:46 +0200

    init commit

---
+++ src/test/java/nl/mds/springcloud/SpringCloudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mds.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
